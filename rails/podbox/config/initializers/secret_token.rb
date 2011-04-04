d1467014732be38ffab870a0fb9fa3485c135cc3
Fernando Henrique <liquuid@liquuid.net>
2011-04-03 21:38:33 -0300

    rails app

---
+++ rails/podbox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podbox::Application.config.secret_token = 'f1a0715b6a4a56bf8817b2769463001a0825e6cac592c13e4b7bd94c5d8af2b3d8dd3adb6c9f15715f389aedf3370ce66d38688f5f6053076aa442582c67fff8'
