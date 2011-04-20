25ff6ccb9ca49be1b8a91a909dba6bbbe5b02f91
Fernando Henrique <liquuid@liquuid.net>
2011-04-19 23:33:25 -0300

    sproutcore

---
+++ sc/todos/apps/todos/controllers/tasks.js
@@ -0,0 +1,57 @@
+// ==========================================================================
+// Project:   Todos.tasksController
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.ArrayController
+*/
+Todos.tasksController = SC.ArrayController.create(
+   SC.CollectionViewDelegate,
+        /** @scope Todos.tasksController.prototype */ {
+    summary: function(){
+     var len = this.get('length'), ret ;
+
+     if (len  && len > 0){
+        ret = len === 1 ? "1 task" : "%@ tasks".fmt(len) ;
+        }else
+        ret = "No tasks";
+
+        return ret;
+    }.property('length').cacheable(),
+
+    collectionViewDeleteContent: function(view, content, indexes) {
+        var records = indexes.map(function(idx) {
+            return this.objectAt(idx);
+        }, this);
+        records.invoke('destroy');
+
+        var selIndex = indexes.get('min')-1;
+        if (selIndex<0) selIndex = 0;
+        this.selectObject(this.objectAt(selIndex));
+    },
+    addTask: function() {
+        var task;
+        task = Todos.store.createRecord(Todos.Task, {
+                "description": "New Task",
+                "isDone": false
+        });
+        this.selectObject(task);
+        this.invokeLater(function() {
+                var contentIndex = this.indexOf(task);
+                var list = Todos.mainPage.getPath('mainPane.middleView.contentView')
+                var listItem = list.itemViewForContentIndex(contentIndex);
+                listItem.beginEditing();
+            });
+            return YES;
+          },
+    toggleDone: function() {
+        var sel = this.get('selection');
+        sel.setEach('isDone', !sel.everyProperty('isDone'));
+        return YES;
+    }
+}) ;
---
+++ sc/todos/apps/todos/fixtures/task.js
@@ -0,0 +1,31 @@
+// ==========================================================================
+// Project:   Todos.Task Fixtures
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+sc_require('models/task');
+
+Todos.Task.FIXTURES = [
+
+   { "guid": "task-1",
+     "description": "Michael",
+     "isDone": false },
+  
+   {"guid": "task-2",
+     "description": "Dwight",
+     "isDone": false },
+  
+   {"guid": "task-3",
+     "description": "Jim",
+     "isDone": false },
+  
+   {"guid": "task-4",
+     "description": "Pam",
+     "isDone": false },
+  
+   {"guid": "task-5",
+     "description": "Ryan",
+     "isDone": false }
+
+];
