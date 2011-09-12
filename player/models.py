b90864489c8038c93bc99755bc2a20f287d18c51
Fernando Silva <fernando@liquuid.net>
2011-09-11 23:01:19 -0300

    todos testes passando, metodos para pegar infos dos feeds e salvar no banco, refatorando alguns metodos para serem privados

--- player/models.py
+++ player/models.py
@@ -1,6 +1,8 @@
 import feedparser
 from urllib2 import urlopen
 from hashlib import md5
+from datetime import datetime
+from time import strptime, mktime
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -21,39 +23,64 @@ post_save.connect(create_user_profile, sender=User)
 
 class Feed(models.Model):
     url = models.CharField(max_length = 256, unique=True)
+    link = models.CharField(max_length = 256)
+    description = models.TextField(max_length = 1024)
     title = models.CharField(max_length = 64, null=True, blank=True)
-    raw_feed = "";
+    pubdate = models.DateTimeField('Date of publication',null=True, blank=True)
+    
+    _raw_feed = "";
     # ??
     silent = models.BooleanField(default = True)
     
-    def get_feed(self):
+    def _get_feed(self):
         try:
-            if not self.raw_feed:
-                self.raw_feed = open("feeds/%s" % md5(self.url).hexdigest(), 'r').read()
+            if not self._raw_feed:
+                self._raw_feed = open("feeds/%s" % md5(self.url).hexdigest(), 'r').read()
         except:
             fd = open("feeds/%s" % md5(self.url).hexdigest(), 'w')
-            self.raw_feed = urlopen(self.url).read()
-            fd.write(self.raw_feed)
+            self._raw_feed = urlopen(self.url).read()
+            fd.write(self._raw_feed)
             fd.close()
             
-    def get_description(self):
-        fparser = feedparser.parse(self.raw_feed)
+    def _get_description(self):
+        fparser = feedparser.parse(self._raw_feed)
         return fparser.feed.description
 
-    def get_title(self):
-        fparser = feedparser.parse(self.raw_feed)
+    def _get_title(self):
+        fparser = feedparser.parse(self._raw_feed)
         return fparser.feed.title
 
-    def get_link(self):
-        fparser = feedparser.parse(self.raw_feed)
+    def _get_link(self):
+        fparser = feedparser.parse(self._raw_feed)
         return fparser.feed.link
 
-    def get_pubdate(self):
-        fparser = feedparser.parse(self.raw_feed)
+    def _get_pubdate(self):
+        fparser = feedparser.parse(self._raw_feed)
         try:
             return fparser.feed.updated
         except:
             return fparser.entries[0].updated
+    
+    def _save_description(self):
+        self.description = self._get_description()
+        self.save()
+
+    def _save_link(self):
+        self.link = self._get_link()
+        self.save()
+
+    def _save_title(self):
+        self.title = self._get_title()
+        self.save()
+    
+    def _save_pubdate(self):
+        self.pubdate = self._rfc2datetime
+        self.save()
+     
+    def _rfc2datetime(self):
+        time_struct = strptime(self._get_pubdate().split('-')[0].strip(), "%a, %d %b %Y %H:%M:%S")
+        return datetime.fromtimestamp(mktime(time_struct))
+        
 
     def __str__(self):
         if self.title:
--- player/tests.py
+++ player/tests.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import datetime
 from django.test import TestCase
 from player.models import *
+
 
 class FeedTest(TestCase):
     def setUp(self):
@@ -13,6 +15,7 @@ class FeedTest(TestCase):
         self.feed_link2 = "http://www.rapaduracast.com.br"
         self.feed_description2 = u"O RapaduraCast \xe9 o podcast do portal Cinema Com Rapadura! De uma forma descontra\xedda, o RapaduraCast traz o conte\xfado do site que ser\xe1 discutido, al\xe9m de algumas novidades e quadros engra\xe7ad\xedssimos. Aqui, n\xe3o iremos medir as palavras ou fazer m\xe9dia, afinal, antes de tudo, somos meros cin\xe9filos mortais como voc\xeas leitores."
         self.feed_pubdate2 = "Wed, 31 Aug 2011 05:30:18 -0300"
+        self.feed_pubdate_iso2 = "2011-08-31T05:30:18"
         self.feed_fake_xml1 = open("feeds/_teste1","r").read()
         self.feed_fake_xml2 = open("feeds/_teste2","r").read()
 
@@ -20,52 +23,57 @@ class FeedTest(TestCase):
         pass
 
     def test_get_title(self):
-        self.feed.raw_feed = self.feed_fake_xml1
-        self.assertEquals(self.feed.get_title(), self.feed_title1)
-        self.feed.raw_feed = self.feed_fake_xml2
-        self.assertEquals(self.feed.get_title(), self.feed_title2)
+        self.feed._raw_feed = self.feed_fake_xml1
+        self.assertEquals(self.feed._get_title(), self.feed_title1)
+        self.feed._raw_feed = self.feed_fake_xml2
+        self.assertEquals(self.feed._get_title(), self.feed_title2)
 
     def test_get_link(self):
-        self.feed.raw_feed = self.feed_fake_xml1
-        self.assertEquals(self.feed.get_link(), self.feed_link1)
-        self.feed.raw_feed = self.feed_fake_xml2
-        self.assertEquals(self.feed.get_link(), self.feed_link2)
+        self.feed._raw_feed = self.feed_fake_xml1
+        self.assertEquals(self.feed._get_link(), self.feed_link1)
+        self.feed._raw_feed = self.feed_fake_xml2
+        self.assertEquals(self.feed._get_link(), self.feed_link2)
 
     def test_get_description(self):
-        self.feed.raw_feed = self.feed_fake_xml1
-        self.assertEquals(self.feed.get_description(), self.feed_description1)
-        self.feed.raw_feed = self.feed_fake_xml2
-        self.assertEquals(self.feed.get_description(), self.feed_description2)
+        self.feed._raw_feed = self.feed_fake_xml1
+        self.assertEquals(self.feed._get_description(), self.feed_description1)
+        self.feed._raw_feed = self.feed_fake_xml2
+        self.assertEquals(self.feed._get_description(), self.feed_description2)
 
     def test_get_pubdate(self):
-        self.feed.raw_feed = self.feed_fake_xml1
-        self.assertEquals(self.feed.get_pubdate(), self.feed_pubdate1)
-        self.feed.raw_feed = self.feed_fake_xml2
-        self.assertEquals(self.feed.get_pubdate(), self.feed_pubdate2)
+        self.feed._raw_feed = self.feed_fake_xml1
+        self.assertEquals(self.feed._get_pubdate(), self.feed_pubdate1)
+        self.feed._raw_feed = self.feed_fake_xml2
+        self.assertEquals(self.feed._get_pubdate(), self.feed_pubdate2)
 
     def test_save_title(self):
-        title = self.feed.title
-        self.feed.title = self.feed_title1
-        self.feed.save_title()
-        self.assertEquals(self.title, self.feed_title1)
+        title = self.feed_title1
+        self.feed._get_title = lambda: title
+        self.feed._save_title()
+        feed_test = Feed.objects.latest('id')
+        self.assertEquals(feed_test.title, self.feed_title1)
 
     def test_save_link(self):
-        link = self.feed.link
-        self.feed.link = self.feed_link1
-        self.feed.save_link()
-        self.assertEquals(self.link, self.feed_link1)
+        link = self.feed_link1
+        self.feed._get_link = lambda: link
+        self.feed._save_link()
+        feed_test = Feed.objects.latest('id')
+        self.assertEquals(feed_test.link, self.feed_link1)
 
     def test_save_description(self):
-        description = self.feed.description
-        self.feed.description = self.feed_description1
-        self.feed.save_description()
-        self.assertEquals(self.description, self.feed_description1)
+        #import pdb; pdb.set_trace()
+        description = self.feed_description1
+        self.feed._get_description = lambda: description
+        self.feed._save_description()
+        feed_test = Feed.objects.latest('id')
+        self.assertEquals(feed_test.description, self.feed_description1)
 
     def test_save_pubdate(self):
-        pubdate = self.feed.pubdate
-        self.feed.pubdate = self.feed_pubdate1
-        self.feed.save_pubdate()
-        self.assertEquals(self.pubdate, self.feed_pubdate1)
+        #pubdate = self.feed_pubdate2
+        self.feed._rfc2datetime = datetime(2011, 8, 31, 5, 30,18)
+        self.feed._save_pubdate()
+        feed_test = Feed.objects.latest('id')
+        self.assertEquals(feed_test.pubdate.isoformat(), self.feed_pubdate_iso2)
 
     def test_url(self):
         self.assertEquals(self.feed.url, "http://fuuu/")
@@ -76,3 +84,6 @@ class FeedTest(TestCase):
         self.feed.title = "Name"
         self.assertEquals(self.feed.__str__(), self.feed.title)
 
+    def test_rfc2datetime(self):
+        self.feed._get_pubdate = lambda: self.feed_pubdate2
+        self.assertEquals(self.feed._rfc2datetime().isoformat(), self.feed_pubdate_iso2)
