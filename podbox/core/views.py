5550a48475d7ecd7410ac9ceca79c811e792e0e7
Fernando Silva <liquuid@gmail.com>
2016-08-29 21:26:55 -0300

    eliminando o eval do parser

--- podbox/core/views.py
+++ podbox/core/views.py
@@ -15,74 +15,71 @@ def index(request):
 
 
 def feed_ws(request):
-    feeds = '{'
+    feeds = {}
     for feed in UserProfile.objects.get(user_id=request.user.id).feeds.values():
-        # import pdb; pdb.set_trace()
-        feeds += " '%s':{ 'id':'%s', 'url':'%s', 'title':'%s'  }," % (
-        feed['id'], feed['id'], feed['url'], feed['title'])
-    feeds += '}'
-    return HttpResponse(json.dumps(eval(feeds)))
+        feeds[feed['id']] = {'id': feed['id'],
+                             'url': feed['url'],
+                             'title': feed['title']}
 
+    return HttpResponse(json.dumps(feeds))
 
 def user_feed_ws(request):
-    feeds = '{'
+    feeds = {}
     for feed in UserProfile.objects.get(user_id=request.user.id).feeds.values():
-        # import pdb; pdb.set_trace()
-        feeds += " '%s':{ 'id':'%s', 'url':'%s', 'silent':'%s', 'title':'%s'  }," % (
-        feed['id'], feed['id'], feed['url'], feed['silent'], feed['title'])
-    feeds += '}'
-    return HttpResponse(json.dumps(eval(feeds)))
+        feeds[feed['id']] = {'id': feed['id'],
+                              'url': feed['url'],
+                              'silent': feed['silent'],
+                              'title': feed['title']}
+
+    return HttpResponse(json.dumps(feeds))
 
 
 def episodes_time_line(request):
-    # episodes = '{'
-    episodes = '{'
+    episodes = {}
     episode_list = Episode.objects.none()
     for feed in UserProfile.objects.get(user_id=request.user.id).feeds.all():
         episode_list = Episode.objects.filter(feeds=feed) | episode_list
 
     for episode in episode_list.order_by('-updated').values():
-        episodes += " '%s':{'id':'%s', 'url':'%s', 'updated':'%s','summary':'%s'  , 'title':'%s' , 'feed_id':'%s' }," % (
-        episode['id'], episode['id'], episode['url'], episode['updated'], episode['summary'], episode['title'],
-        episode['feeds_id'])
-        print(episode['id'])
+        episodes[episode['id']] = {'id': episode['id'],
+                                   'url': episode['url'],
+                                   'updated': episode['updated'].strftime("%d/%m/%Y"),
+                                   'summary': episode['summary'],
+                                   'title': episode['title'],
+                                   'feed_id': episode['feeds_id']
+                                   }
 
-    # print episode_list.order_by('-updated')
-    episodes += '}'
-    return HttpResponse(json.dumps(eval(episodes)))
+    return HttpResponse(json.dumps(episodes))
 
 
 def episodes_playlist(request):
-    episodes = []
     episode_list = Episode.objects.none()
     for feed in UserProfile.objects.get(user_id=request.user.id).feeds.all():
         episode_list = Episode.objects.filter(feeds=feed) | episode_list
 
-    for episode in episode_list.order_by('-updated').values():
-        episodes.append(episode['id'])
+    episodes = [episode['id'] for episode in episode_list.order_by('-updated').values()]
 
     return HttpResponse(json.dumps(episodes))
 
 
 def user_episodes_time_line(request):
-    episodes = '{'
+    episodes = {}
     episode_list = Episode.objects.none()
     for feed in UserProfile.objects.get(user_id=request.user.id).feeds.all():
         episode_list = Episode.objects.filter(feeds=feed) | episode_list
 
     for episode in episode_list.order_by('-updated').values():
-        episodes += " '%s':{'id':'%s', 'url':'%s', 'updated':'%s','summary':'%s'  , 'title':'%s' , 'feed_id':'%s' }," % (
-        episode['id'], episode['id'], episode['url'], episode['updated'], episode['summary'], episode['title'],
-        episode['feeds_id'])
-        print(episode)
+        episodes[episode['id']] = {'id': episode['id'],
+                                   'url': episode['url'],
+                                   'updated': episode['updated'].strftime("%d/%m/%Y"),
+                                   'summary': episode['summary'],
+                                   'title': episode['title'],
+                                   'feed_id': episode['feeds_id']
+                                   }
 
-    # print episode_list.order_by('-updated')
-    episodes += '}'
-    return HttpResponse(json.dumps(eval(episodes)))
-
+    return HttpResponse(json.dumps(episodes))
 
 def update_feed(request, feed_id):
     feed = Feed.objects.get(id=feed_id)
     feed.update_episodes()
     return HttpResponse("feito")
-
